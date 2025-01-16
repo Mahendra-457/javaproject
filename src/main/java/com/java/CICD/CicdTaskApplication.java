b02f50b1572ac9054ecbd17ffdeffe546cd2b2c5
Mahendra Reddy <mahendrareddy@Mahendras-MacBook-Air.local>
2025-01-16 15:54:27 -0600

    code

---
+++ src/main/java/com/java/CICD/CicdTaskApplication.java
@@ -0,0 +1,13 @@
+package com.java.CICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTaskApplication.class, args);
+	}
+
+}
